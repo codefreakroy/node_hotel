61073e68ea3ecc2c32b7132b3cffd8987f52d270
apurv-iifetech <apurv@iifetech.com>
2024-04-23 16:54:18 +0530

    add comment

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express()
 app.use(bodyParser.json()) // req.body
 
 const port = process.env.PORT || 3000
-
 app.listen(port, () => {
     console.log('Server is running on port 3000')
 })
